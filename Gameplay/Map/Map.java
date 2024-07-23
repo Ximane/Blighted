b23da934c74788cbf05ab0ef850b6229d0832a1c
Maximillian <mh01@tamu.edu>
2024-07-23 15:13:21 -0500

    Minor changes; unimportant

---
+++ Gameplay/Map/Map.java
@@ -0,0 +1,26 @@
+// import java.awt.*;
+// import javax.swing.*;
+
+
+
+// public class Map extends JFrame {
+
+//     public void initialize(){
+
+//         JPanel mainPanel = new JPanel();
+//         mainPanel.setLayout(new BorderLayout());
+//         mainPanel.setBackground(new Color(128, 128, 255));
+
+//         setTitle("Game");
+//         // setSize(500, 600);
+//         setExtendedState(JFrame.MAXIMIZED_BOTH);
+//         setMinimumSize(new Dimension(300,400));
+//         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//         setVisible(true);
+//     }
+
+//     public static void main(String args[]){
+//         Map field = new Map();
+//         field.initialize();
+//     }
+// }
