8293bae59eb7f0d6f5b0d9f84f9f909ca4c73158
Maximillian <mh01@tamu.edu>
2024-07-25 13:20:31 -0500

    Made Home Page more functional and augmented  aesthetically pleasing capabilities

--- HomeScreen/Home.java
+++ HomeScreen/Home.java
@@ -5,58 +5,106 @@ import javax.swing.*;
 public class Home {
 
     private JFrame homePage;
+
+    private JPanel mainPanel;
+    private JPanel FirstBlock;
+    private JPanel SecondBlock;
+    private JPanel ThirdBlock;
+    private JPanel FourthBlock;
+    private JPanel FifthBlock;
+    private JPanel SixthBlock;
+
     private JButton play;
     private JButton settings;
     private JButton quit;
-    private JPanel mainPanel;
-    private JPanel holder;
+    private JButton back;
 
+    
+    
+    //Function to set the default settings for the JFrame
+    public void defaultFrameSetting(JFrame frame){
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setMinimumSize(new Dimension(700, 500));
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+    //Function to Make Buttons Blend with Background
+    public void BlendedButton(JButton button){
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+    }
+
+    //Initialize the Home Screen
     public void initialize(){
         homePage = new JFrame();
         homePage.setTitle("Home");
-        homePage.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        homePage.setMinimumSize(new Dimension(700,500));
-        homePage.setLocationRelativeTo(null);
-        homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        defaultFrameSetting(homePage);
 
-        // getContentPane().add(homePage);
+        //Create the Play Button
         play = new JButton("Play");
-
+        BlendedButton(play);
+        
+        //Create Settings Button
         settings = new JButton("Settings");
+        BlendedButton(settings);
+        settings.addActionListener(e->{
+            SecondBlock.remove(play);
+            ThirdBlock.remove(settings);
+            FourthBlock.remove(quit);
+            
+            //Back Button for Settings Page
+            back = new JButton("Back");
+            BlendedButton(back);
+            back.addActionListener(f->{
+                SixthBlock.remove(back);
+                SecondBlock.add(play);
+                ThirdBlock.add(settings);
+                FourthBlock.add(quit);
+                defaultFrameSetting(homePage);
+            });
+            SixthBlock.add(back);
 
+            // homePage.pack();
+            defaultFrameSetting(homePage);
+        });
+
+        //Quit Buttom Implementation
         quit = new JButton("Quit");
+        BlendedButton(quit);
         quit.addActionListener(e -> {
             homePage.dispose(); //Exit on click
          });
         
-
-        // // setBackground(new Color(128, 128, 255));
-        // setTitle("Home");
-        // // setSize(500, 600);
-        // setExtendedState(JFrame.MAXIMIZED_BOTH);
-        // setMinimumSize(new Dimension(700,500));
-        // setLocationRelativeTo(null);
-        // setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-
+         //Create the Panels that are viewed
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayout(6,3));
         mainPanel.setBackground(new Color(128, 128, 255));
 
-        holder = new JPanel();
+        FirstBlock = new JPanel();
+        SecondBlock = new JPanel();
+        ThirdBlock = new JPanel();
+        FourthBlock = new JPanel();
+        FifthBlock = new JPanel();
+        SixthBlock = new JPanel();
         // JPanel options = new JPanel();
         // options.setLayout(new BoxLayout(options, BoxLayout.PAGE_AXIS));
         // options.setBackground(new Color(255));
 
-        // mainPanel.add(options);
-        mainPanel.add(holder);
-        mainPanel.add(play);
-        mainPanel.add(settings);
-        mainPanel.add(quit);
+        SecondBlock.add(play);
+        ThirdBlock.add(settings);
+        FourthBlock.add(quit);        
+
+        mainPanel.add(FirstBlock);
+        mainPanel.add(SecondBlock);
+        mainPanel.add(ThirdBlock);
+        mainPanel.add(FourthBlock);
+        mainPanel.add(FifthBlock);
+        mainPanel.add(SixthBlock);
+
         homePage.add(mainPanel);
         
-
         homePage.setVisible(true);
     }
 
