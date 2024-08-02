1bf4df1cd011ee58a6b8d0d7dd30ef1dfbf88291
Maximillian <mh01@tamu.edu>
2024-08-02 17:21:27 -0500

    Outlined the lobby components

--- Gameplay/Lobby/GraphicsPanel.java
+++ Gameplay/Lobby/GraphicsPanel.java
@@ -7,43 +7,77 @@ public class GraphicsPanel extends JPanel{
 
     int panelHeight;
     int panelWidth;
+    int circle;
     
     @Override
     //Function to draw on the frame to create the terrain outlines
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D graphics = (Graphics2D) g;
+        // graphics.fillRect(x, y, Width, Height);
 
         getDimensions(this);
 
         //Research Table
         graphics.setColor(Color.PINK);
-        graphics.drawRect(0, (int)(panelHeight*0.55), (int)(panelWidth*0.075), (int)(panelHeight*0.3));
-        graphics.fillRect(0, (int)(panelHeight*0.55), (int)(panelWidth*0.075), (int)(panelHeight*0.3));
+        graphics.drawRect(0, (int)(panelHeight*0.65), (int)(panelWidth*0.05), (int)(panelHeight*0.2));
 
         //Entertainment Shop NPC
+        graphics.setColor(Color.BLACK);
+        graphics.drawOval((int)(panelWidth*0.04), (int)(panelHeight*0.3), circle, circle);
         
-        //Stools + Window - See the music and backgrounds collected
+        //Stools + Window - See the music and backgrounds collected; Backdrop
+        graphics.setColor(new Color(150, 222, 209)); //Robin Egg Blue
+        graphics.drawRect(0, 0, (int)(panelWidth), (int)(panelHeight*0.15));
+        graphics.drawRect((int)(panelWidth*0.3), 0, (int)(panelWidth*0.05), (int)(panelHeight*0.15)); //Pillar1
+        graphics.drawRect((int)(panelWidth*0.65), 0, (int)(panelWidth*0.05), (int)(panelHeight*0.15)); //Pillar2
+
+        // - Window
+        graphics.setColor(Color.WHITE);
+        graphics.drawRect((int)(panelWidth*0.025),(int)(panelHeight*0.025), (int)(panelWidth*0.2), (int)(panelHeight*0.1));
         
+        // - Stools
+        graphics.drawRect((int)(panelWidth*0.04), (int)(panelHeight*0.175), (int)(panelWidth*0.03), (int)(panelHeight*0.07));
+        graphics.drawRect((int)(panelWidth*0.075), (int)(panelHeight*0.175), (int)(panelWidth*0.03), (int)(panelHeight*0.07));
+        graphics.drawRect((int)(panelWidth*0.11), (int)(panelHeight*0.175), (int)(panelWidth*0.03), (int)(panelHeight*0.07));
+        graphics.drawRect((int)(panelWidth*0.145), (int)(panelHeight*0.175), (int)(panelWidth*0.03), (int)(panelHeight*0.07));
+        graphics.drawRect((int)(panelWidth*0.18), (int)(panelHeight*0.175), (int)(panelWidth*0.03), (int)(panelHeight*0.07));
+
+
         //NPC 1 - Temp Buff
+        graphics.setColor(Color.WHITE);
+        graphics.drawOval(panelWidth*21/36, panelHeight*11/18, circle, circle);
 
         //NPC 2
+        graphics.drawOval(panelWidth*14/36, panelHeight*7/18, circle, circle);
 
         //NPC 3
+        graphics.drawOval(panelWidth*21/36, panelHeight*7/18, circle, circle);
 
         //Play
+        graphics.setColor(Color.WHITE);
+        graphics.drawOval((int)((panelWidth-circle*2.5)/2), (int)((panelHeight*1.05-circle*2.5)/2), circle*3, circle*3);
 
         //Item Box
+        graphics.drawRect(panelWidth/3, panelHeight*11/18, panelWidth/10, panelHeight/10);
 
         //Spawn Box - Can make small carpet
+        graphics.drawRect((int)(panelWidth*0.4), (int)(panelHeight*0.9), (int)(panelWidth*0.2), (int)(panelHeight*0.1));
+
+        //Exit Door
+        graphics.drawRect((int)(panelWidth*0.2), (int)(panelHeight*0.95), (int)(panelWidth*0.1), (int)(panelHeight*0.05));
 
         //Random Event Corner - Just need the coordinates moreso than anything
+        graphics.drawRect((int)(panelWidth*0.875), (int)(panelHeight*0.825), (int)(panelWidth*0.125), (int)(panelHeight*0.175));
 
         //Costume closet
+        graphics.drawRect((int)(panelWidth*0.965), (int)(panelHeight*0.6), (int)(panelWidth*0.035), (int)(panelHeight*0.175));
 
-        //Exchange COunter
+        //Exchange Counter
+        graphics.drawRect((int)(panelWidth*0.93), (int)(panelHeight*0.4), (int)(panelWidth*0.07), (int)(panelHeight*0.125));
 
         //Shady Gambling
+        graphics.drawRect((int)(panelWidth*0.9), (int)(panelHeight*0.15), (int)(panelWidth*0.1), (int)(panelHeight*0.15));
     
     }
 
@@ -55,6 +89,7 @@ public class GraphicsPanel extends JPanel{
 
         panelHeight = pane.getHeight();
         panelWidth = pane.getWidth();
+        circle = (int)(panelHeight*0.075);
 
         // System.out.print(panelWidth + " " + panelHeight);
     }
